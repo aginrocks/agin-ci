e7e61851b24946af634e6294b1cf05f32ce29747
Tymek <vonautymek@gmail.com>
2025-07-02 12:37:19 +0200

    docs: moved the docs to the root path

--- docs/lib/source.ts
+++ docs/lib/source.ts
@@ -5,7 +5,7 @@ import { attachFile, createOpenAPI } from "fumadocs-openapi/server";
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   // it assigns a URL to your pages
-  baseUrl: "/docs",
+  baseUrl: "/",
   source: docs.toFumadocsSource(),
   pageTree: {
     // adds a badge to each page item in page tree

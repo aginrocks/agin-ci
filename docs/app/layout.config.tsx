ef90abb28b036b99b8c4f5549ca11c42bea668b8
Tymek <vonautymek@gmail.com>
2025-07-03 08:56:06 +0200

    docs: added logo

--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
 /**
  * Shared layout configurations
@@ -8,21 +9,26 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-  nav: {
-    title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
-      </>
-    ),
-  },
-  // see https://fumadocs.dev/docs/ui/navigation/links
-  links: [],
+    nav: {
+        title: (
+            <>
+                <Image
+                    src="/logo.svg"
+                    alt="Logo"
+                    width={100}
+                    height={60}
+                    className="hidden dark:block"
+                />
+                <Image
+                    src="/logo-light.svg"
+                    alt="Logo"
+                    width={100}
+                    height={60}
+                    className="dark:hidden"
+                />
+            </>
+        ),
+    },
+    // see https://fumadocs.dev/docs/ui/navigation/links
+    links: [],
 };

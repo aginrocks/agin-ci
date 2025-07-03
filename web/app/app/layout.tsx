981d3c8612dc9a1562dfdeb4e5c4c6a949839775
Tymek <vonautymek@gmail.com>
2025-07-03 11:41:33 +0200

    web: fixed collapsed state

--- web/app/app/layout.tsx
+++ web/app/app/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarInset, SidebarProvider } from '@components/ui/sidebar';
+import { SidebarProvider } from '@components/ui/sidebar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return <SidebarProvider>{children}</SidebarProvider>;

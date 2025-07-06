db0053ea2d689a6fa7342ad88c73d8ff012079e8
Tymek <vonautymek@gmail.com>
2025-07-06 13:34:16 +0200

    web: added header background

--- web/components/landing-page/header/index.tsx
+++ web/components/landing-page/header/index.tsx
@@ -2,11 +2,26 @@ import { Logo } from '@components/logo';
 import { HeaderLink } from './link';
 import { Button } from '@components/ui/button';
 import { DOCS_URL, REPO_URL } from '@lib/constants';
+import { useWindowScroll } from '@mantine/hooks';
+import { cn } from '@lib/utils';
+import clsx from 'clsx';
 
 export function Header() {
+    const [{ y }] = useWindowScroll();
+
+    const hasBackground = y > 100;
+
     return (
-        <div className="fixed top-0 left-0 right-0 p-6 flex justify-center items-center z-50">
-            <div className="flex justify-between items-center w-full max-w-7xl">
+        <div className="fixed top-0 left-0 right-0 p-3 flex justify-center items-center z-50">
+            <div
+                className={cn(
+                    'flex justify-between items-center w-full max-w-7xl p-3 rounded-full border border-transparent transition-all duration-500',
+                    clsx({
+                        'bg-neutral-100 dark:bg-neutral-800/20 backdrop-blur-xl border-border':
+                            hasBackground,
+                    })
+                )}
+            >
                 <div className="flex gap-4 items-center">
                     <Logo className="p-0" href="/" />
                     <div className="flex items-center mb-[2px] gap-0.5">

cb6a458c22ee632c2146907c0afc6f037306fa9f
Tymek <vonautymek@gmail.com>
2025-07-06 13:09:16 +0200

    web: added bototm gradient to hero

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
         >
             <Header />
             <Hero />
+            <div className="h-screen"></div>
         </div>
     );
 }
--- web/components/landing-page/hero/hero-background.tsx
+++ web/components/landing-page/hero/hero-background.tsx
@@ -5,13 +5,14 @@ export function HeroBackground() {
         <div className="absolute inset-0 flex h-full w-full items-center justify-center">
             <div
                 className={cn(
-                    'absolute inset-0 -z-2',
+                    'absolute inset-0 -z-3',
                     '[background-size:30px_30px]',
                     '[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]',
                     'dark:[background-image:radial-gradient(#323232_1px,transparent_1px)]'
                 )}
             />
             <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-background -z-1"></div>
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:linear-gradient(to_bottom,transparent_85%,black_100%)] dark:bg-background -z-2"></div>
         </div>
     );
 }

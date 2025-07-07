5e009da5fe3714eeeb2041d4783f4588d90625d4
Tymek <vonautymek@gmail.com>
2025-07-07 20:42:58 +0200

    server: changed ssh link format to use ssh:// (in order to store custom ports)

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -11,8 +11,14 @@ pub fn normalize_git_url(url: &str) -> Result<String> {
     let user = parsed_url.user.unwrap_or("git".to_string());
     let host = parsed_url.host.wrap_err("Missing host in the URL")?;
     let fullname = parsed_url.fullname;
+    let mut port = parsed_url.port.unwrap_or(22).to_string();
+    if port == "22" {
+        port = "".to_string();
+    } else {
+        port = format!(":{}", port);
+    }
 
-    let normalized_url = format!("{user}@{host}:{fullname}.git");
+    let normalized_url = format!("ssh://{user}@{host}{port}/{fullname}.git");
 
     Ok(normalized_url)
 }

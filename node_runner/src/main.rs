4678f0265904d4bfda28d7db07cfe056c076aeb5
Tymek <vonautymek@gmail.com>
2025-07-16 14:35:13 +0200

    node_runner, librunner: boilerplate code

--- node_runner/src/main.rs
+++ node_runner/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
+use color_eyre::eyre::{Context, Result};
+use tracing::{info, level_filters::LevelFilter};
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{fmt::format::FmtSpan, layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    init_tracing().wrap_err("failed to set global tracing subscriber")?;
+
+    info!(
+        "Starting {} {}...",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION"),
+    );
+
     println!("Hello, world!");
+
+    Ok(())
+}
+
+fn init_tracing() -> Result<()> {
+    tracing_subscriber::Registry::default()
+        .with(tracing_subscriber::fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE))
+        .with(ErrorLayer::default())
+        .with(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .with_env_var("RUST_LOG")
+                .from_env()?,
+        )
+        .try_init()?;
+
+    Ok(())
 }

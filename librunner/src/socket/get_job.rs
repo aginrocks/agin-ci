1b9d78633191b0f0395ea2017e3c47a838599df6
Tymek <vonautymek@gmail.com>
2025-07-17 15:09:58 +0200

    librunner: added ack

--- librunner/src/socket/get_job.rs
+++ librunner/src/socket/get_job.rs
@@ -1,13 +1,21 @@
+use serde_json::{Value, json};
 use socketioxide::{
     SocketIo,
-    extract::{Extension, SocketRef},
+    extract::{AckSender, Extension, SocketRef},
 };
 use tracing::info;
 
 use crate::socket::UserData;
 
-pub fn handler(socket: SocketRef, io: SocketIo, Extension(data): Extension<UserData>) {
-    let name = data.0.job.name.unwrap_or("Unknown".to_string());
+pub async fn handler(
+    socket: SocketRef,
+    io: SocketIo,
+    ack: AckSender,
+    Extension(data): Extension<UserData>,
+) {
+    let name = data.0.job.clone().name.unwrap_or("Unknown".to_string());
 
     info!("job: {name}");
+
+    ack.send(&data.0.job).ok();
 }
